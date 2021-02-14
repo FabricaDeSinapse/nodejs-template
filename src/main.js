c6f02e08ef9e7f432bd502c5ddfe640890ae29c1
Paulo Salvatore <salvatoregames@gmail.com>
2021-02-14 20:34:13 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const variavelNumerica = '2';
 
 if (variavelNumerica === 2 && variavelNumerica === 2) {
-    console.info('data');
-    console.info(`data: ${variavelNumerica}`);
+    console.info('data 2');
+            console.info('data: ' + variavelNumerica);
 }
